5ca75124c6536b90e06bc0656823eaa6d1fae726
Vladislav <vladislav.lifirenko@gmail.com>
2022-10-24 18:45:17 +0300

    Initial commit

---
+++ Assets/Scripts/Ecs/System/Unit/RotationSystem.cs
@@ -0,0 +1,31 @@
+﻿using Apache.Ecs.Component.Unit;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Apache.Ecs.System.Unit
+{
+    public class RotationSystem : IEcsRunSystem
+    {
+        private readonly EcsFilterInject<Inc<UnitComponent, RotationComponent>> _filter;
+        private readonly EcsPoolInject<UnitComponent> _unitPool;
+        private readonly EcsPoolInject<RotationComponent> _rotationPool;
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var entity in _filter.Value)
+            {
+                var rotation = _rotationPool.Value.Get(entity);
+                if (rotation.Direction == Vector3.zero)
+                    return;
+
+                var unit = _unitPool.Value.Get(entity);
+
+                unit.View.Transform.rotation = Quaternion.Slerp(
+                    unit.View.Transform.rotation,
+                    Quaternion.LookRotation(rotation.Direction, unit.View.Transform.up),
+                    Time.deltaTime * rotation.Speed);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Config/CommonConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Apache.Model.Config
+{
+    [CreateAssetMenu(fileName = "Common", menuName = "Config/Common Config")]
+    public class CommonConfig : ScriptableObject
+    {
+        public LayerMask groundLayer;
+    }
+}
